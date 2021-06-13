05f895f148138cda6879912856b92e96dbf2ea83
kunal jaykam <kunaljaykam@icloud.com>
2021-06-13 16:06:39 +0530

    typescript-level-1

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    console.log(typeof n1 === 'number');
+    return n1 + n2;
+}
+;
+add(2, 2);
+console.log(add);
---
+++ app.ts
@@ -0,0 +1,9 @@
+function add(n1, n2) {
+console.log(typeof n1 === 'number')
+return n1 + n2
+};
+
+add(2 , 2);
+
+console.log(add)
+
